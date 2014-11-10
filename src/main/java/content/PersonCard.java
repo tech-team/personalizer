a66eecf02a3c3c516a4a9c24d50b332e268c806c
igorcoding <igorcoding@gmail.com>
2014-11-10 22:42:13 +0300

    Made some chages to ContentData

--- src/main/java/content/PersonCard.java
+++ src/main/java/content/PersonCard.java
@@ -3,7 +3,8 @@ package content;
 import java.util.EnumMap;
 import java.util.Map;
 
-public class ContentData {
+public class PersonCard {
+    private int id = -1;
     private Map<SocialLink.LinkType, String> name = new EnumMap<>(SocialLink.LinkType.class);
     private Map<SocialLink.LinkType, String> surname = new EnumMap<>(SocialLink.LinkType.class);
     private Map<SocialLink.LinkType, Integer> ageFrom = new EnumMap<>(SocialLink.LinkType.class);
@@ -12,7 +13,7 @@ public class ContentData {
     private Map<SocialLink.LinkType, Boolean> famous = new EnumMap<>(SocialLink.LinkType.class);
     private Map<SocialLink.LinkType, SocialLink> socialLinks = new EnumMap<>(SocialLink.LinkType.class);
 
-    public ContentData(Map<SocialLink.LinkType, String> name, Map<SocialLink.LinkType, String> surname, Map<SocialLink.LinkType, Integer> ageFrom, Map<SocialLink.LinkType, Integer> ageTo, Map<SocialLink.LinkType, String> email, Map<SocialLink.LinkType, Boolean> famous, Map<SocialLink.LinkType, SocialLink> socialLinks) {
+    public PersonCard(Map<SocialLink.LinkType, String> name, Map<SocialLink.LinkType, String> surname, Map<SocialLink.LinkType, Integer> ageFrom, Map<SocialLink.LinkType, Integer> ageTo, Map<SocialLink.LinkType, String> email, Map<SocialLink.LinkType, Boolean> famous, Map<SocialLink.LinkType, SocialLink> socialLinks) {
         this.name = name;
         this.surname = surname;
         this.ageFrom = ageFrom;
@@ -22,7 +23,11 @@ public class ContentData {
         this.socialLinks = socialLinks;
     }
 
-    public ContentData() {
+    public PersonCard(int id) {
+        this.id = id;
+    }
+
+    public PersonCard() {
     }
 
     public Map<SocialLink.LinkType, String> getNameMap() {
@@ -84,38 +89,46 @@ public class ContentData {
 
 
 
-    public ContentData setName(SocialLink.LinkType linkType, String name) {
+    public PersonCard setName(SocialLink.LinkType linkType, String name) {
         this.name.put(linkType, name);
         return this;
     }
 
-    public ContentData setSurname(SocialLink.LinkType linkType, String surname) {
+    public PersonCard setSurname(SocialLink.LinkType linkType, String surname) {
         this.surname.put(linkType, surname);
         return this;
     }
 
-    public ContentData setAgeFrom(SocialLink.LinkType linkType, Integer ageFrom) {
+    public PersonCard setAgeFrom(SocialLink.LinkType linkType, Integer ageFrom) {
         this.ageFrom.put(linkType, ageFrom);
         return this;
     }
 
-    public ContentData setAgeTo(SocialLink.LinkType linkType, Integer ageTo) {
+    public PersonCard setAgeTo(SocialLink.LinkType linkType, Integer ageTo) {
         this.ageTo.put(linkType, ageTo);
         return this;
     }
 
-    public ContentData setEmail(SocialLink.LinkType linkType, String email) {
+    public PersonCard setEmail(SocialLink.LinkType linkType, String email) {
         this.email.put(linkType, email);
         return this;
     }
 
-    public ContentData setFamous(SocialLink.LinkType linkType, Boolean isFamous) {
+    public PersonCard setFamous(SocialLink.LinkType linkType, Boolean isFamous) {
         this.famous.put(linkType, isFamous);
         return this;
     }
 
-    public ContentData setSocialLink(SocialLink.LinkType linkType, SocialLink link) {
+    public PersonCard setSocialLink(SocialLink.LinkType linkType, SocialLink link) {
         this.socialLinks.put(linkType, link);
         return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
