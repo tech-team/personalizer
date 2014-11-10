36d4698bfded1a62ed30cf1553c7d4d0adbd104f
Сергей <seregachern@mail.ru>
2014-11-10 22:44:18 +0300

    VK start

--- src/main/java/content/source/vk/VK.java
+++ src/main/java/content/source/vk/VK.java
@@ -3,9 +3,22 @@ package content.source.vk;
 import content.ContentData;
 import content.source.ContentSource;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class VK implements ContentSource {
+
+    public static final String 
+
     @Override
     public ContentData retrieve(ContentData data) {
+
         return null;
     }
+
+    public void getInfo(String name, String surName){
+        HttpURLConnection httpConnection = (HttpURLConnection) new URL(url).openConnection();
+        httpConnection.setRequestMethod("POST");
+    }
+
 }
