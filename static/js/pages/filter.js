3064f936d8c5579328b3cce7831a1aac8e26a239
alekseyl1992 <alekseyl@list.ru>
2014-11-15 16:36:43 +0300

    Drag & Drop workarounds

--- static/js/pages/filter.js
+++ static/js/pages/filter.js
@@ -1,4 +1,38 @@
 
 $(document).ready(function() {
+    $(".sources .column").sortable({
+        connectWith: ".destinations .column",
+        handle: ".portlet-header",
+        cancel: ".portlet-toggle",
+        placeholder: "portlet-placeholder ui-corner-all",
 
+        receive: function(e, ui) {
+            //TODO: test is it possible to place that item that column
+            $(ui.sender).sortable("cancel");
+        },
+
+//                stop: function()
+//                {
+//                    $(this).sortable('cancel');
+//                }
+    });
+
+    $(".destinations .column").sortable({
+        connectWith: ".sources .column",
+        handle: ".portlet-header",
+        cancel: ".portlet-toggle",
+        placeholder: "portlet-placeholder ui-corner-all"
+    });
+
+    $(".portlet")
+        .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+        .find(".portlet-header")
+        .addClass("ui-widget-header ui-corner-all")
+        .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+    $(".portlet-toggle").click(function () {
+        var icon = $(this);
+        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
+        icon.closest(".portlet").find(".portlet-content").toggle();
+    });
 });
