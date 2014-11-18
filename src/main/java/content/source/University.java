7700ea010316db179163eaa5c90293cb615c2552
Сергей <seregachern@mail.ru>
2014-11-18 19:18:49 +0300

    merged

--- src/main/java/content/source/University.java
+++ src/main/java/content/source/University.java
@@ -1,7 +1,23 @@
 package content.source;
 
-/**
- * Created by Sergey on 18.11.14.
- */
+
 public class University {
+    private String name;
+    private String graduation;
+
+    public University(String name){
+        this.name = name;
+        this.graduation = null;
+    }
+
+    public University(String name, String graduation){
+        this.name = name;
+        this.graduation = graduation;
+    }
+
+    public University(String name, Integer graduation){
+        this.name = name;
+        if (graduation != null)
+            this.graduation = graduation.toString();
+    }
 }
