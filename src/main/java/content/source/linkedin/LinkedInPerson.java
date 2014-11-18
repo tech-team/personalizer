0dd070c57950dfeccfd9063e7be0318a4461b4db
Max Myalkin <maxmyalkin@gmail.com>
2014-11-18 23:59:04 +0300

    Linkedin. +UserPageObject. Parsing main info

--- src/main/java/content/source/linkedin/LinkedInPerson.java
+++ src/main/java/content/source/linkedin/LinkedInPerson.java
@@ -47,12 +47,4 @@ public class LinkedInPerson {
     public void setUrl(String url) {
         this.url = url;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
