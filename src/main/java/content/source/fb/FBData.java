2cda4e881a955ade8f4c3ef56ac1ed34b13fa073
Abashinos <abashinos@gmail.com>
2014-11-23 00:08:59 +0300

    Fb data retrieval - name and avatar. Access token yet to be received from httpGet

---
+++ src/main/java/content/source/fb/FBData.java
@@ -0,0 +1,15 @@
+package content.source.fb;
+
+/**
+ * Created by Solid on 22.11.2014.
+ */
+public class FBData {
+
+    static final String appUrl = "http://localhost:8083";
+    static final String appId = "1565265913709814";
+    static final String loginResponseType = "token";
+    static final String loginUrl = "https://www.facebook.com/dialog/oauth";
+    static final String baseUrl = "https://graph.facebook.com/v2.2/";
+    static final String pic = "/picture?redirect=false&height=150&width=150";
+
+}
